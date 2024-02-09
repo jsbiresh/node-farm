1b484a81fed55903eb8860423ed268dca0c89581
Biresh <js.biresh@gmail.com>
2024-02-09 16:13:33 +0530

    finished Non-Blocking Asynchronous Way

--- starter/index.js
+++ starter/index.js
@@ -14,3 +14,27 @@ console.log(textOut);
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log("File has been written!")
 
+console.log("--------------------------------------------------")
+
+// Non-Blocking Asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err1, data1) => {
+    if (err1) return console.log("ERROR!")
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err2, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err3, data3) => {
+            // console.log(`${data2}. \n${data3}`)
+            // console.log(data3)
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log("File Reading and Writing completed.")
+            })
+        })
+    })
+    
+    // if (err) {
+    //     console.log(`${err}, Found Error.` )
+    //     return
+    // }
+    // console.log(data1)
+})
+
+console.log('will read file')
