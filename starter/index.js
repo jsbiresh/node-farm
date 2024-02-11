ebb369d4a0a98dca8aedca53c046f8dab91f0fa0
Biresh <js.biresh@gmail.com>
2024-02-11 14:15:22 +0530

    moved replaceTemplate function to make it a module

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,10 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const slugify = require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // =======================================================================================================
 
@@ -46,73 +50,70 @@ const url = require("url");
 
 // http SERVER
 
-
-
-
-
 //
 // Synchronous code executed only once, when the machine starts
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
-//
+
+const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
-  
-  const { query, pathname } = url.parse(req.url, true)
+  const { query, pathname } = url.parse(req.url, true);
 
-//   const pathName = req.url;
+  //   const pathName = req.url;
+  //   const pathName = req.url;
 
   // OVERVIEW PAGE
-  if (pathname === "/" || pathname === "/overview") {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
-      "Content-type": "text/html",
+      'Content-type': 'text/html',
     });
-    const cardsHtml = dataObject.map((el) => replaceTemplate(tempCard, el)).join('')
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
-    res.end(output)
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     // PRODUCTS PAGE
-  } else if (pathname === "/product") {
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const product = dataObject[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-        res.writeHead(200, {
-            "Content-type": "text/html",
-        });
-        const product = dataObject[query.id]
-        const output = replaceTemplate(tempProduct, product)
-
-        res.end(output);
-
+    res.end(output);
 
     // FILE READ PAGE
-  } else if (pathname == "/filec") {
-    fs.readFile("./txt/output.txt", "utf-8", (err, data) => {
+  } else if (pathname == '/filec') {
+    fs.readFile('./txt/output.txt', 'utf-8', (err, data) => {
       if (err) {
-        console.error("ERROR:", err);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
+        console.error('ERROR:', err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
         return;
       }
-      console.log(data + " data");
-      res.writeHead(200, { "Content-Type": "text/plain" });
+      console.log(data + ' data');
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(data);
     });
 
-
     // API PAGE
-  } else if (pathname === "/api") {
+  } else if (pathname === '/api') {
     // this code has been moved up to the top, for global access.
     // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
     //     const productData = JSON.parse(data)
@@ -125,27 +126,25 @@ const server = http.createServer((req, res) => {
     // res.end('API DATA')
 
     res.writeHead(200, {
-      "Content-type": "application/json",
+      'Content-type': 'application/json',
     });
     res.end(data);
-
 
     // PAGE NOT FOUND
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello-world",
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
-    res.end("<h1>No Page found for the URL.</h1>");
+    res.end('<h1>No Page found for the URL.</h1>');
   }
 });
 
 // Use process.env.PORT or default to 3000
 const port = process.env.PORT || 8000;
 
-server.listen(port, "127.0.0.1", () => {
+server.listen(port, '127.0.0.1', () => {
   console.log(`Running on PORT: ${port}`);
 });
-
 
 // ===========================================================
